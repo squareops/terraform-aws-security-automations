5bb0551782b7b7ce71b7e6e38f7859742e88b89d
Ajay-sops <ajay@squareops.com>
2023-08-29 17:31:00 +0530

    main.tf changes

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -33,7 +33,7 @@ module "cis" {
   remove_ssl_tls_iam                  = false
   enable_guard_duty                   = true
   enable_security_hub                 = true
-  mfa_iam_group_name                  = "mfa-group"                                                                   ## enter your user group name
+  mfa_iam_group_name                  = "" ## enter your IAM user group for mfa                                                                  ## enter your user group name
   cloudwatch_logs_kms_key_arn         = "" ## enter kms key id for encrypting cloudwatch log group of cloud trail
   cloudwatch_log_group_retention_days = 60
 }
