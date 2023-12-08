2157b76514d338578267190503233a0fea56b927
amanmallsops <github@squareops.com>
2023-12-08 17:17:00 +0530

    fixing in s3 bucket lifecycle in soc2

--- modules/soc2/main.tf
+++ modules/soc2/main.tf
@@ -109,7 +109,7 @@ resource "aws_s3_bucket" "gd_bucket" {
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
-  bucket = aws_s3_bucket.gd_bucket.id
+  bucket = aws_s3_bucket.gd_bucket[0].id
   rule {
     id = "guardduty_s3"
     expiration {
